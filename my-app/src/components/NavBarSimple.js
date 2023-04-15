27b686e87133fc128b889b19a7399d93fe8dae6a
Sediqa <sadat.hadid2000@gmail.com>
2023-04-15 09:56:21 +0000

    Add program structure

---
+++ my-app/src/components/NavBarSimple.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+class NavBarSimple extends React.Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+
+        };
+    }
+
+
+
+    render(){
+        return (
+            <p>HTML Component Here</p>
+        );
+
+    }
+    
+}
+
+export default NavBarSimple
